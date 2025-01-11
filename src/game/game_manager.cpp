2836e24f7ff7669271b139c7d2b1b8fa83c7f779
aintDatCap <default.pentagon042@passinbox.com>
2025-01-11 14:14:42 +0100

    Game manager destructor

--- src/game/game_manager.cpp
+++ src/game/game_manager.cpp
@@ -1,6 +1,7 @@
 #include "game/game_manager.hpp"
 #include "game/graphics.hpp"
 #include "game/logic.hpp"
+#include <ncurses.h>
 namespace Snake {
 
 SnakeGameManager::SnakeGameManager(uint16_t window_width, uint16_t window_height, List<LevelInfo> levels) {
@@ -9,22 +10,36 @@ SnakeGameManager::SnakeGameManager(uint16_t window_width, uint16_t window_height
     this->menu_ui = new MenuUI(window_width, window_height, [this]() { this->start_game(); });
 }
 
+SnakeGameManager::~SnakeGameManager() {
+    if (this->game) {
+        delete this->game;
+    }
+
+    if (this->game_ui) {
+        delete this->game_ui;
+    }
+
+    if (this->menu_ui) {
+        delete this->menu_ui;
+    }
+}
+
 void SnakeGameManager::start_game() {
-    
+
     delete this->menu_ui;
     this->game = new Game(window_height, window_width);
     this->game_ui = new GameUI(this->game);
-    
-    //TODO: timer, player input
+
+    // TODO: timer, player input
     Direction player_direction = this->get_player_input();
     while (game->update_game(player_direction) != GAME_UNFINISHED) {
         game_ui->update_game_window();
-        // timer
-        #ifdef _WIN32
-        // windows timer
-        #else
-        // linux timer
-        #endif
+// timer
+#ifdef _WIN32
+// windows timer
+#else
+// linux timer
+#endif
     }
 
     this->next_level();
@@ -34,7 +49,11 @@ void SnakeGameManager::stop_game() {
     delete this->game;
     delete this->game_ui;
     this->menu_ui = new MenuUI(window_width, window_height, [this]() { this->start_game(); });
+}
 
+Direction SnakeGameManager::get_player_input() {
+    // TODO
+    return DIRECTION_UP;
 }
 
 void SnakeGameManager::next_level() {
